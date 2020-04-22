4875ab84ba13bdab7205aa49770b7ccc4ef0df0a
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-04-22 05:39:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,8 @@
 
 def reverse_each_word(string)
   array = string.split
-  reversed_words = []
-  array.each do |word|
-    reversed_words << word.reverse
+  array.collect do |word|
+   word.reverse
   end
   reversed_words.join(" ")
 end
